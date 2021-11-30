df20a896fc46f824260194edef5fd7ac15062342
trevorm4 <trevorm4@illinois.edu>
2021-11-30 00:08:56 -0600

    test

--- analysis/monkmistweaver/src/modules/spells/EssenceFont.tsx
+++ analysis/monkmistweaver/src/modules/spells/EssenceFont.tsx
@@ -116,7 +116,7 @@ class EssenceFont extends Analyzer {
     }
     this.expected_duration = (3000 + extra_secs * 1000) / (1 + this.haste.current);
     this.last_ef = event.timestamp;
-    this.castEF += 2;
+    this.castEF += 3;
     this.total += this.uniqueTargets.size || 0;
     this.uniqueTargets.clear();
   }
